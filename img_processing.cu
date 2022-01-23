5e8250391aec20a9554a94d7b4dee639789d600a
0xArchy <52278758+0xArchy@users.noreply.github.com>
2022-01-23 16:34:21 +0100

    Update img_processing.cu

--- img_processing.cu
+++ img_processing.cu
@@ -21,15 +21,16 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
-/* CUDA kernel. Each thread takes care of one element of src */
-__global__ void kernel_conv_grey(uint8_t *src, uint8_t *dst, int width, int height) {
+/*GPU kernel functions*/
+__global__ void kernel_filter_grey(uint8_t *src, uint8_t *dst, int width, int height) {
 	int i, j, k, l;
-	/* Init static filter */
+	/* kernel filter */
 	int h[3][3] = {{1, 2, 1}, {2, 4, 2}, {1, 2, 1}};
-	/* get position */
+
 	size_t x = blockIdx.x*blockDim.x + threadIdx.x;
 	size_t y = blockIdx.y*blockDim.y + threadIdx.y;
-	/* convolute */
+
+	/* Aplicamos el filtro */
 	if (0 < x && x < height-1 && 0 < y && y < width-1) {
 		float val = 0;
 		for (i = x-1, k = 0 ; i <= x+1 ; i++, k++)
@@ -39,14 +40,15 @@ __global__ void kernel_conv_grey(uint8_t *src, uint8_t *dst, int width, int heig
 	}
 }
 
-__global__ void kernel_conv_rgb(uint8_t *src, uint8_t *dst, int width, int height) {
+__global__ void kernel_filter_rgb(uint8_t *src, uint8_t *dst, int width, int height) {
 	int i, j, k, l;
-	/* Init static filter */
+	/* kernel filter */
 	int h[3][3] = {{1, 2, 1}, {2, 4, 2}, {1, 2, 1}};
-	/* get position */
+
 	size_t x = blockIdx.x*blockDim.x + threadIdx.x;
 	size_t y = blockIdx.y*blockDim.y + threadIdx.y;
-	/* convolute */
+
+	/* Aplicamos el filtro */
 	if (0 < x && x < height-1 && 0 < y && y < 3*width-3) {
 		float redval = 0, greenval = 0, blueval = 0;
 		for (i = x-1, k = 0 ; i <= x+1 ; i++, k++) {
@@ -62,41 +64,37 @@ __global__ void kernel_conv_rgb(uint8_t *src, uint8_t *dst, int width, int heigh
 	}
 }
 
-extern "C" void gpuConvolute(uint8_t *src, int width, int height, int loops, color_t imageType)
+extern "C" void gpuFilter(uint8_t *src, int width, int height, int loops, color_t imageType)
 {
-	/* Device vectors */
 	uint8_t *d_src, *d_dst, *tmp;
 	size_t bytes = (imageType == GREY) ? height * width : height * width*3;
 
-	/* Allocate memory for each vector on GPU */
     CUDA_SAFE_CALL( cudaMalloc(&d_src, bytes * sizeof(uint8_t)) );
     CUDA_SAFE_CALL( cudaMalloc(&d_dst, bytes * sizeof(uint8_t)) );
  
-    /* Copy host vectors to device memory */
     CUDA_SAFE_CALL( cudaMemcpy(d_src, src, bytes, cudaMemcpyHostToDevice) );
     CUDA_SAFE_CALL( cudaMemset(d_dst, 0, bytes) );
 
     int t;
 	const int blockSize = 16;
-	/* Convolute "loops" times */
+	/*Repetimos el bucle loops veces (que indica el usuario)*/
 	for (t = 0 ; t < loops ; t++) {
 		
 		if (imageType == GREY) {
-			/* Specify layout of Grid and Blocks */
 			int gridX = FRACTION_CEILING(height, blockSize);
 			int gridY = FRACTION_CEILING(width, blockSize);
 			dim3 block(blockSize, blockSize);
 			dim3 grid(gridX, gridY);
-			kernel_conv_grey<<<grid, block>>>(d_src, d_dst, width, height);
+			kernel_filter_grey<<<grid, block>>>(d_src, d_dst, width, height);
 		} else if (imageType == RGB) {
 			int gridX = FRACTION_CEILING(height, blockSize);
 			int gridY = FRACTION_CEILING(width*3, blockSize);
 			dim3 block(blockSize, blockSize);
 			dim3 grid(gridX, gridY);
-			kernel_conv_rgb<<<grid, block>>>(d_src, d_dst, width, height);
+			kernel_filter_rgb<<<grid, block>>>(d_src, d_dst, width, height);
 		}
 
-		/* swap arrays */
+		/* intercambiamos los arrays */
 		tmp = d_src;
 	    d_src = d_dst;
 	    d_dst = tmp;
@@ -105,14 +103,13 @@ extern "C" void gpuConvolute(uint8_t *src, int width, int height, int loops, col
 	CUDA_SAFE_CALL( cudaGetLastError() );
 	CUDA_SAFE_CALL( cudaThreadSynchronize() );
     
-    /* Copy array back to host */
+    /* Copiamos el array de vuelta al host */
     if (loops%2 == 0) {
     	CUDA_SAFE_CALL( cudaMemcpy(src, d_src, bytes, cudaMemcpyDeviceToHost) );
     } else {
    		CUDA_SAFE_CALL( cudaMemcpy(src, d_dst, bytes, cudaMemcpyDeviceToHost) );
    	}
 
-	// Release device memory
     CUDA_SAFE_CALL( cudaFree(d_src) );
     CUDA_SAFE_CALL( cudaFree(d_dst) );
 }
