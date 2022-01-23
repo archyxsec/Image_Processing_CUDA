0a4c1ce2db50503cf13fed59941d377e2f31a47b
0xArchy <52278758+0xArchy@users.noreply.github.com>
2022-01-23 16:33:55 +0100

    Update functions.h

--- functions.h
+++ functions.h
@@ -20,25 +20,33 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdint.h>
+#include <assert.h>
+#include <sys/time.h>
 #include <time.h>
-#include "funcs.h"
+#include "functions.h"
+
+/*CUDA Safe Call define, para controlar errores*/
 
 #define CUDA_SAFE_CALL(call) {                                    		\
     cudaError err = call;												\
     if( cudaSuccess != err) {                                           \
-        fprintf(stderr, "Cuda error in file '%s' in line %i : %s.\n",   \
+        fprintf(stderr, "Cuda error en fichero '%s' en linea %i : %s.\n",   \
                 __FILE__, __LINE__, cudaGetErrorString( err) );         \
         exit(EXIT_FAILURE);                                             \
     } }
 
 #define FRACTION_CEILING(numerator, denominator) ((numerator+denominator-1)/denominator)
 
+/*Estructura para el tipo de fotografía, en gris o color*/
 typedef enum {RGB, GREY} color_t;
 
-int write_all(int, uint8_t *, int);
-int read_all(int, uint8_t *, int);
-void Usage(int, char **, char **, int *, int *, int *, color_t *);
-uint64_t micro_time(void);
+int write_img(int, uint8_t *, int);
+int read_img(int, uint8_t *, int);
+void Uso(int, char **, char **, int *, int *, int *, color_t *);
+uint64_t time_m(void);
 
 #endif
